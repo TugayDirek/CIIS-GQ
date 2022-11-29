8477b1a40e04d6efef9251bd335ee73e6ae0a03f
TugayDirek <tugaydirek@hotmail.com>
2022-11-29 17:44:01 +0300

    Update main_cmd.py

--- main_cmd.py
+++ main_cmd.py
@@ -8,30 +8,30 @@ print()
 file = sys.argv[1]
 file_name = str(file).split("\\")[-1].split(".")[0]
 print("Name of the structure: " + file_name)
-atomsDNA, atomsRNA, atomsDNA_sequence, atomsRNA_sequence = g4_depictor.readFromFiles([file])
+atomsDNA, atomsRNA, atomsDNA_sequence, atomsRNA_sequence = CIIS_GQ.readFromFiles([file])
 # print(atomsDNA)
 # print(atomsRNA)
 # print(atomsDNA_sequence)
 
-g4_depictor.convertKeywordsOneComma("FL","DG",atomsDNA)
-g4_depictor.convertKeywordsOneComma("OG","DG",atomsDNA)
-g4_depictor.convertKeywordsOneComma("F2","DG",atomsDNA)
-g4_depictor.convertKeywordsOneComma("CG","DG",atomsDNA)
-g4_depictor.convertKeywordsOneComma("F0","DG",atomsDNA)
-g4_depictor.convertKeywordsOneComma("GM","DG",atomsDNA)
-g4_depictor.convertKeywordsOneComma("0G","DG",atomsDNA)
+CIIS_GQ.convertKeywordsOneComma("FL","DG",atomsDNA)
+CIIS_GQ.convertKeywordsOneComma("OG","DG",atomsDNA)
+CIIS_GQ.convertKeywordsOneComma("F2","DG",atomsDNA)
+CIIS_GQ.convertKeywordsOneComma("CG","DG",atomsDNA)
+CIIS_GQ.convertKeywordsOneComma("F0","DG",atomsDNA)
+CIIS_GQ.convertKeywordsOneComma("GM","DG",atomsDNA)
+CIIS_GQ.convertKeywordsOneComma("0G","DG",atomsDNA)
 
-g4_depictor.convertKeywordsOneComma("G","DG",atomsDNA)
+CIIS_GQ.convertKeywordsOneComma("G","DG",atomsDNA)
 
-g4_depictor.convertKeywordsOneComma("FL","DG",atomsRNA)
-g4_depictor.convertKeywordsOneComma("OG","DG",atomsRNA)
-g4_depictor.convertKeywordsOneComma("F2","DG",atomsRNA)
-g4_depictor.convertKeywordsOneComma("CG","DG",atomsRNA)
-g4_depictor.convertKeywordsOneComma("F0","DG",atomsRNA)
-g4_depictor.convertKeywordsOneComma("GM","DG",atomsRNA)
-g4_depictor.convertKeywordsOneComma("0G","DG",atomsRNA)
+CIIS_GQ.convertKeywordsOneComma("FL","DG",atomsRNA)
+CIIS_GQ.convertKeywordsOneComma("OG","DG",atomsRNA)
+CIIS_GQ.convertKeywordsOneComma("F2","DG",atomsRNA)
+CIIS_GQ.convertKeywordsOneComma("CG","DG",atomsRNA)
+CIIS_GQ.convertKeywordsOneComma("F0","DG",atomsRNA)
+CIIS_GQ.convertKeywordsOneComma("GM","DG",atomsRNA)
+CIIS_GQ.convertKeywordsOneComma("0G","DG",atomsRNA)
 
-g4_depictor.convertKeywordsOneComma("G","DG",atomsRNA)
+CIIS_GQ.convertKeywordsOneComma("G","DG",atomsRNA)
 
 atoms = {}
 atoms_sequence = {}
@@ -46,19 +46,19 @@ elif len(atomsRNA.keys()) != 0:
 print("-----------")
 angleDict = {}
 
-bondVectors = g4_depictor.calculateDistances_AngleMethod(atoms, max_bond_distance=5)
-guaninePlanes = g4_depictor.findNormals(atoms)
-angleDict_narrowRange = g4_depictor.findHBondGuaninePlaneNormalAngle(bondVectors, guaninePlanes, 40)
-angleDict_wideRange = g4_depictor.findHBondGuaninePlaneNormalAngle(bondVectors, guaninePlanes, 70)
+bondVectors = CIIS_GQ.calculateDistances_AngleMethod(atoms, max_bond_distance=5)
+guaninePlanes = CIIS_GQ.findNormals(atoms)
+angleDict_narrowRange = CIIS_GQ.findHBondGuaninePlaneNormalAngle(bondVectors, guaninePlanes, 40)
+angleDict_wideRange = CIIS_GQ.findHBondGuaninePlaneNormalAngle(bondVectors, guaninePlanes, 70)
 
 
 tetrads_byAngle = {}
-tetrads_byAngle = g4_depictor.findTetrads_AngleMethod(angleDict_narrowRange)
+tetrads_byAngle = CIIS_GQ.findTetrads_AngleMethod(angleDict_narrowRange)
 # print(tetrads_byAngle)
 if(any(tetrads_byAngle.values())):
-    wideRangeAngleList = g4_depictor.checkTetradCycle(tetrads_byAngle, angleDict_narrowRange, angleDict_wideRange)
-    g4_depictor.deleteImproperTetrads(tetrads_byAngle)
-    ordered_tetrads = g4_depictor.orderTetrads(tetrads_byAngle, atoms)
+    wideRangeAngleList = CIIS_GQ.checkTetradCycle(tetrads_byAngle, angleDict_narrowRange, angleDict_wideRange)
+    CIIS_GQ.deleteImproperTetrads(tetrads_byAngle)
+    ordered_tetrads = CIIS_GQ.orderTetrads(tetrads_byAngle, atoms)
 
     # order tetrads from min to max through from bottom to top
     for key in ordered_tetrads.keys():
@@ -67,8 +67,8 @@ if(any(tetrads_byAngle.values())):
             ordered_tetrads[key] = ordered_tetrads[key][::-1]
 
     loops_byAngle = {}
-    loops_byAngle = g4_depictor.findLoops(tetrads_byAngle, angleDict_narrowRange, angleDict_wideRange, wideRangeAngleList, atoms, reversed=False)
-    ONZ,tetrad_onz = g4_depictor.ONZ(ordered_tetrads, angleDict_narrowRange, angleDict_wideRange, wideRangeAngleList, atoms)
+    loops_byAngle = CIIS_GQ.findLoops(tetrads_byAngle, angleDict_narrowRange, angleDict_wideRange, wideRangeAngleList, atoms, reversed=False)
+    ONZ,tetrad_onz = CIIS_GQ.ONZ(ordered_tetrads, angleDict_narrowRange, angleDict_wideRange, wideRangeAngleList, atoms)
 
     print("Tetrads and Loops: ")
     test_set = {}
@@ -76,9 +76,9 @@ if(any(tetrads_byAngle.values())):
     for key in loops_byAngle:
         ligand_enter = False
 
-        dict, ligand_enter = g4_depictor.listG4Structure(key, ordered_tetrads, loops_byAngle, atoms_sequence)
+        dict, ligand_enter = CIIS_GQ.listG4Structure(key, ordered_tetrads, loops_byAngle, atoms_sequence)
         test_set.update(dict)
 
-    g4_depictor.drawStructure(test_set, file_name, ONZ, tetrad_onz, atoms_sequence)
+    CIIS_GQ.drawStructure(test_set, file_name, ONZ, tetrad_onz, atoms_sequence)
 else:
     print("It is not a G4 structure")
